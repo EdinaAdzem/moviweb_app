04cb3aebbbc998e041fa2947f6b75d334fb90fcd
EdinaAdzem <71989436+EdinaAdzem@users.noreply.github.com>
2024-09-17 15:44:12 +0200

    adding template files to correspond to all the set up routes

--- app.py
+++ app.py
@@ -20,13 +20,22 @@ def home():
 @app.route('/users')
 def list_users():
     users = data_manager.get_all_users()
-    return render_template('users.html', users=users)
+    return render_template('users_page.html', users=users)
 
 
 @app.route('/user/<int:user_id>/movies')
 def user_movies(user_id):
     movies = data_manager.get_user_movies(user_id)
     return render_template('movies.html', movies=movies, user_id=user_id)
+
+
+@app.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        user_name = request.form['name']
+        data_manager.add_user({'name': user_name})
+        return redirect(url_for('list_users'))
+    return render_template('add_user.html')
 
 
 @app.route('/user/<int:user_id>/movies/add', methods=['GET', 'POST'])
@@ -44,20 +53,20 @@ def add_movie(user_id):
     return render_template('add_movie.html')
 
 
-@app.route('/user/<int:user_id>/movies/update/<int:movie_id>', methods=['GET', 'POST'])
+@app.route('/users/<int:user_id>/update_movie/<int:movie_id>', methods=['GET', 'POST'])
 def update_movie(user_id, movie_id):
+    movie = data_manager.get_movie(movie_id)
     if request.method == 'POST':
         movie_data = {
             'name': request.form['name'],
             'director': request.form['director'],
             'year': request.form['year'],
-            'rating': request.form['rating']
+            'rating': request.form['rating'],
+            'user_id': user_id
         }
         data_manager.update_movie(movie_id, movie_data)
         return redirect(url_for('user_movies', user_id=user_id))
-
-    movie = Movie.query.get(movie_id)
-    return render_template('update_movie.html', movie=movie, user_id=user_id)
+    return render_template('update_movie.html', movie=movie)
 
 
 @app.route('/user/<int:user_id>/movies/delete/<int:movie_id>')
